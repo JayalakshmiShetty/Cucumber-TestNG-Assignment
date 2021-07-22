dab8adf5787d1795f0ccce3d3d7cf16ed7c6c713
JayalakshmiShetty <laxmi.mite@gmail.com>
2021-07-22 11:54:15 +0400

    modified pom.xml

--- src/test/java/runner/Default.java
+++ src/test/java/runner/Default.java
@@ -4,7 +4,8 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(glue = {"stepdefs"}, plugin = {"html:target/cucumber-reports/cucumber-pretty","json:target/json-cucumber-reports/default/cukejson.json",
+@CucumberOptions(glue = {"stepdefs"}, plugin = {"html:target/cucumber-reports/cucumber-pretty",
+		"json:target/json-cucumber-reports/default/cukejson.json",
 		"testng:target/testng-cucumber-reports/cuketestng.xml" }, features = {"src/test/resources/features"})
 public class Default extends AbstractTestNGCucumberParallelTests {
 	
